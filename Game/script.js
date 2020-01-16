5e7c5dcd0d6f3397a2bf14a8ccdacc9341d08468
Kamil Lubowicki <lubowickik@outlook.com>
2020-01-16 22:02:18 +0100

    added falling cat

--- Game/script.js
+++ Game/script.js
@@ -5,6 +5,7 @@ let gameStarted = false;
 
 const superDog = document.getElementById('superDog');
 const world = document.getElementById('world');
+const fallingCat = document.getElementById('fallingCat');
 
 
 const worldHeight = parseInt(window.getComputedStyle(world).height);
@@ -14,9 +15,16 @@ const worldWidth = parseInt(window.getComputedStyle(world).width);
 const superDogWidth = parseInt(window.getComputedStyle(superDog).width);
 const superDogHeight = parseInt(window.getComputedStyle(superDog).height);
 
+const fallingCatWidth = parseInt(window.getComputedStyle(fallingCat).width);
+const fallingCatHeight = parseInt(window.getComputedStyle(fallingCat).height);
+
 let superDogSpeed = 10;
 let superDogPositionX = parseInt(window.getComputedStyle(superDog).left);
 let superDogPositionY = parseInt(window.getComputedStyle(superDog).bottom);
+
+let fallingCatPositionX = parseInt(window.getComputedStyle(fallingCat).right);
+let fallingCatPositionY = parseInt(window.getComputedStyle(fallingCat).bottom);
+
 
 function startGame() {
     gameStarted=true;
@@ -42,12 +50,12 @@ window.addEventListener('keydown', event => {
     console.log('event: ', event.code);
     if (event.code === 'ArrowRight' && superDogPositionX + superDogWidth < worldWidth) {
         superDogPositionX += superDogSpeed;
-        superDog.style.transform = 'scaleX(1)';
+        superDog.style.transform = 'scaleX(-1)';
         superDog.style.left = `${superDogPositionX}px`;
     }
     if (event.code === 'ArrowLeft' && superDogPositionX > 0) {
         superDogPositionX -= superDogSpeed;
-        pirate.style.transform = 'scaleX(-1)';
+        superDog.style.transform = 'scaleX(1)';
         superDog.style.left = `${superDogPositionX}px`;
     }
     if (event.code === 'ArrowUp' && superDogPositionY + superDogHeight < worldHeight) {
@@ -56,3 +64,29 @@ window.addEventListener('keydown', event => {
     }
 
 });
+
+
+
+
+// function component(width, height, color, x, y) {
+//   this.gamearea = gamearea;
+//   this.width = width;
+//   this.height = height;
+//   this.angle = 0;
+//   this.speed = 1;
+//   this.x = x;
+//   this.y = y;
+//   this.update = function() {
+//     ctx = myGameArea.context;
+//     ctx.save();
+//     ctx.translate(this.x, this.y);
+//     ctx.rotate(this.angle);
+//     ctx.fillStyle = color;
+//     ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+//     ctx.restore();
+//   }
+//   this.newPos = function() {
+//     this.x += this.speed * Math.sin(this.angle);
+//     this.y -= this.speed * Math.cos(this.angle);
+//   }
+// }
