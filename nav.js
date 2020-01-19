ae8ad7a4c876812700ae0046bac4c25da3ac5462
Katarzyna Bes-Laskowska <katarzyna_bes@gazeta.pl>
2020-01-19 12:43:01 +0100

    fixed

--- nav.js
+++ nav.js
@@ -2,28 +2,16 @@
 
 
 const menuLine = document.querySelectorAll(".main-menu li a");
-console.log(menuLine);
 const hamburger = document.querySelector("#page-nav .fa-bars");
-console.log(hamburger);
 const menuList = document.querySelector(".main-menu");
 console.log(menuList);
-const cross = document.querySelector('#page-nav .fa-times');
-console.log(cross);
 const pageNavArea = document.getElementById('page-nav');
-console.log(pageNavArea);
 const headerContainer = document.querySelector('.site-header .container');
-console.log(headerContainer);
-
-
-console.log(menuList);
-
-
 
 menuList.addEventListener("click", function () {
     setTimeout(() => {
         hamburger.style.display = 'block';
         menuList.style.display = 'none';
-        //cross.style.display = 'none'
     headerContainer.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
 
     }, 100);
@@ -33,14 +21,9 @@ menuList.addEventListener("click", function () {
 hamburger.addEventListener("click", function () {
     hamburger.style.display = 'none';
     menuList.style.display = 'block';
+    //menuList.style.toggle('slide');
     headerContainer.style.backgroundColor = "rgba(0, 0, 0, 0.895)";
 });
 
 
-cross.addEventListener("click", function () {
-    hamburger.style.display = 'block';
-    menuList.style.display = 'none';
-
-   
-});
 
